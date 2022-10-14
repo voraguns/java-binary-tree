d3cfb0a2d29e0ff275b360e0845a4dc55ac9d770
Voragun <voragun2543@gmail.com>
2022-10-14 19:35:31 +0700

    Create element and connect the element

--- code/Start.java
+++ code/Start.java
@@ -1,6 +1,18 @@
 class Start {
     public static void main(String[] data) {
-        System.out.println("Hello");
+        Element root = new Element();
+        root.name = "The Sun";
+        root.value = 1;
+        
+        root.left = new Element();
+        root.left.name = "Earth";
+        root.left.value = 7;
+        
+        Element venus = new Element();
+        venus.name = "Venus";
+        venus.value = 12;
+        
+        root.right = venus;
     }
 }
 
